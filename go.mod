53fbf3e514b2a98b5696f853a75463542d313fbf
Luiz Aoqui <luiz@hashicorp.com>
2021-10-30 23:55:33 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/hashicorp/nomad-consul-check
+
+go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/cronexpr v1.1.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/nomad/api v0.0.0-20211029074458-906d94c76fa2
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/hashicorp/nomad/api"
+)
+
+type NodeResp struct {
+	Node   *api.Node
+	NodeID string
+	Error  error
+}
+
+var (
+	shouldHaveConsul  bool
+	shouldBeIneligble bool
+)
+
+func init() {
+	flag.BoolVar(&shouldHaveConsul, "consul", false, "")
+	flag.BoolVar(&shouldBeIneligble, "ineligible", false, "")
+}
+
+func main() {
+	flag.Parse()
+
+	c, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		fmt.Printf("failed to initialize Nomad client: %v", err)
+		os.Exit(1)
+	}
+
+	nodeStubs, _, err := c.Nodes().List(nil)
+	if err != nil {
+		fmt.Printf("failed to initialize Nomad client: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("=> Found %d nodes\n", len(nodeStubs))
+
+	nodeStatus := "Eligible"
+	if shouldBeIneligble {
+		nodeStatus = "Ineligible"
+	}
+	consulStatus := "without"
+	if shouldHaveConsul {
+		consulStatus = "with"
+	}
+	fmt.Printf("=> %s nodes %s Consul:\n\n", nodeStatus, consulStatus)
+
+	var wg sync.WaitGroup
+	wg.Add(len(nodeStubs))
+
+	ch := make(chan NodeResp)
+	for _, n := range nodeStubs {
+		go func(n *api.NodeListStub) {
+			defer wg.Done()
+			node, _, err := c.Nodes().Info(n.ID, nil)
+			ch <- NodeResp{
+				Node:   node,
+				NodeID: n.ID,
+				Error:  err,
+			}
+		}(n)
+	}
+
+	go func() {
+		counter := 1
+		for r := range ch {
+			if r.Error != nil {
+				fmt.Printf("Failed to query node %s", r.NodeID)
+				continue
+			}
+
+			consulVersion := r.Node.Attributes["consul.version"]
+			hasConsul := consulVersion != ""
+			isEligible := r.Node.SchedulingEligibility == "eligible"
+
+			shouldPrint := shouldHaveConsul == hasConsul && shouldBeIneligble == !isEligible
+			if shouldPrint {
+				fmt.Printf("   %s - %s - %s\n", r.Node.Attributes["unique.network.ip-address"], r.Node.Name, r.Node.ID)
+			}
+
+			if counter%50 == 0 {
+				fmt.Printf("Processed %d/%d\n", counter, len(nodeStubs))
+			}
+		}
+	}()
+
+	wg.Wait()
+	close(ch)
+
+	// Wait for stdout to flush.
+	time.Sleep(1 * time.Second)
+}
